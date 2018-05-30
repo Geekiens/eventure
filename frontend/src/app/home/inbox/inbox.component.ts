c7fb48f5c772ea6ac71362963a8d02ad67aa4c2a
Max Brämer <max.braemer@adesso.de>
2018-05-30 15:11:36 +0200

    Anzeige von dem Text der Emails

--- frontend/src/app/home/inbox/inbox.component.ts
+++ frontend/src/app/home/inbox/inbox.component.ts
@@ -14,21 +14,59 @@ import { NotificationsService } from 'angular2-notifications';
 export class InboxComponent implements OnInit {
 
   emails: Email[];
+  selectedEmail: Email = null;
 
   constructor(private notificationsService: NotificationsService) { }
 
-
+  emailClicked(email: Email) {
+    this.selectedEmail = email;
+  }
 
   ngOnInit() {
      this.emails = this.getEmails();
   }
+
 
   getEmails() {
       return [
         {
           absender: 'Peter Müller',
           titel: 'Terminabsprache',
-          text: 'Lorem ipsum dolor sit amet, consetetur sadipscing el diam voluptua. At vero eos et accusam et justo duo dono sea takimata sanctus est Lorem ipsum dolor sit ',
+          text: `Lorem ipsum dolor sit amet, consetetur sadipscing el diam voluptua. At vero eos et accusam
+          sum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam vol
+          uptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor s
+          it amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et 
+          justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscin
+          g elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+           sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+           Lorem ipsum dolor sit amet, consetetur sadipscing el diam voluptua. At vero eos et accusam
+          sum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam vol
+          uptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor s
+          it amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et 
+          justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscin
+          g elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+           sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+           Lorem ipsum dolor sit amet, consetetur sadipscing el diam voluptua. At vero eos et accusam
+          sum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam vol
+          uptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor s
+          it amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et 
+          justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscin
+          g elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+           sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+           Lorem ipsum dolor sit amet, consetetur sadipscing el diam voluptua. At vero eos et accusam
+          sum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam vol
+          uptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor s
+          it amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et 
+          justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscin
+          g elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+           sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+           Lorem ipsum dolor sit amet, consetetur sadipscing el diam voluptua. At vero eos et accusam
+          sum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam vol
+          uptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor s
+          it amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et 
+          justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscin
+          g elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+           sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. `,
           absendeDatum: '2018-05-30',
           priortaet: 'normal',
           erscheintDirekt: true,
@@ -36,7 +74,8 @@ export class InboxComponent implements OnInit {
         {
           absender: 'Peter Müller',
           titel: 'Urlaub',
-          text: 'Lorem ipsum dolor sit amet, consetetur sadipscing el diam voluptua. At vero eos et accusam et justo duo dono sea takimata sanctus est Lorem ipsum dolor sit ',
+          text: `Lorem ipsum dolor sit amet, consetetur sadipscing el diam voluptua. At vero eos et accusam
+          et justo duo dono sea takimata sanctus est Lorem ipsum dolor sit `,
           absendeDatum: '2018-05-30',
           priortaet: 'normal',
           erscheintDirekt: true,
@@ -44,7 +83,8 @@ export class InboxComponent implements OnInit {
         {
           absender: 'Franz Meier',
           titel: 'Gehaltsgespräch',
-          text: 'Lorem ipsum dolor sit amet, consetetur sadipscing el diam voluptua. At vero eos et accusam et justo duo dono sea takimata sanctus est Lorem ipsum dolor sit ',
+          text: `Lorem ipsum dolor sit amet, consetetur sadipscing el diam voluptua. At vero eos et accusam
+          et justo duo dono sea takimata sanctus est Lorem ipsum dolor sit `,
           absendeDatum: '2018-05-30',
           priortaet: 'normal',
           erscheintDirekt: true,
@@ -52,7 +92,8 @@ export class InboxComponent implements OnInit {
         {
           absender: 'Max Mustermann',
           titel: 'Bewerbung',
-          text: 'Lorem ipsum dolor sit amet, consetetur sadipscing el diam voluptua. At vero eos et accusam et justo duo dono sea takimata sanctus est Lorem ipsum dolor sit ',
+          text: `Lorem ipsum dolor sit amet, consetetur sadipscing el diam voluptua. At vero eos et accusam
+          et justo duo dono sea takimata sanctus est Lorem ipsum dolor sit `,
           absendeDatum: '2018-05-30',
           priortaet: 'normal',
           erscheintDirekt: true,
@@ -60,7 +101,8 @@ export class InboxComponent implements OnInit {
         {
           absender: 'Max Mustermann',
           titel: 'Bewerbung2',
-          text: 'Lorem ipsum dolor sit amet, consetetur sadipscing el diam voluptua. At vero eos et accusam et justo duo dono sea takimata sanctus est Lorem ipsum dolor sit ',
+          text: `Lorem ipsum dolor sit amet, consetetur sadipscing el diam voluptua. At vero eos et accusam
+          et justo duo dono sea takimata sanctus est Lorem ipsum dolor sit `,
           absendeDatum: '2018-05-30',
           priortaet: 'normal',
           erscheintDirekt: true,
@@ -68,7 +110,8 @@ export class InboxComponent implements OnInit {
         {
           absender: 'Max Mustermann',
           titel: 'Bewerbung3',
-          text: 'Lorem ipsum dolor sit amet, consetetur sadipscing el diam voluptua. At vero eos et accusam et justo duo dono sea takimata sanctus est Lorem ipsum dolor sit ',
+          text: `Lorem ipsum dolor sit amet, consetetur sadipscing el diam voluptua. At vero eos et accusam
+          et justo duo dono sea takimata sanctus est Lorem ipsum dolor sit `,
           absendeDatum: '2018-05-30',
           priortaet: 'normal',
           erscheintDirekt: true,
